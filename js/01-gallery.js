4eeb976e2417778c90907c928aa72cd68ec0eac9
Dimmac <84150134+Dimmac@users.noreply.github.com>
2021-10-28 02:13:07 +0300

    task1+task2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+// console.log(markup);
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+gallery.addEventListener('click', onSelectImage);
+
+function onSelectImage(event) {
+  event.preventDefault();
+  const image = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${image}" width="800" height="600">`
+  );
+  instance.show();
+  //   instance.show(() => console.log('lightbox now visible'));
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
+
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </li>`;
+  })
+  .join('');
+
+// console.log(markup);
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+// console.log(galleryItems);
